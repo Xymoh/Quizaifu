e93ebebe100910fda0d1ccb54adfca5611cc77b3
Yagami19 <itharil19@gmail.com>
2022-01-25 16:04:23 +0100

    Debug options update
    Updated 2 debug options:
    -adding money
    -resetting values to default

--- Assets/ResetValues.cs
+++ Assets/ResetValues.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class ResetValues : MonoBehaviour
 {
     // Start is called before the first frame update
-    void ResettingValues()
+    public void ResettingValues()
     {
+
+        for (int i = 0; i < SaveManager.instance.chairsUnlocked.Length; i++)
+        {
+            SaveManager.instance.chairsUnlocked[i] = false;
+        }
+        SaveManager.instance.chairsUnlocked[0] = true;
+        SaveManager.instance.currentChair = 0;
+
+        GlobalValues.moneyTxtValue = 0;
+        SaveManager.instance.Save();
         Debug.Log("Resetting values to default...");
 
     }
